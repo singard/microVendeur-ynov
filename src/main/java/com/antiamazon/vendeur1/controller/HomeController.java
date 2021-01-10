1874b9b434411910329cd03172df85031c69c604
Mouhamadou Ndour <taphacr@gmail.com>
2021-01-10 18:14:24 +0100

    Cross Origin

--- src/main/java/com/antiamazon/vendeur1/controller/HomeController.java
+++ src/main/java/com/antiamazon/vendeur1/controller/HomeController.java
@@ -16,6 +16,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 public class HomeController {
 
@@ -33,7 +34,6 @@ public class HomeController {
     	return newVendeur.toString();
     }*/
 
-    @CrossOrigin(origins = "https://guarded-badlands-84396.herokuapp.com")
     @PostMapping(value = "/fill", consumes = "application/json")
     public ResponseEntity register(@RequestBody FillVendeurModel model) {
 
