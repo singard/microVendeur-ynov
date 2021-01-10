01230dc6711b69852e139e92cab9327964af92bf
Mouhamadou Ndour <taphacr@gmail.com>
2021-01-10 05:59:23 +0100

    Ajout de la route de test

--- src/main/java/com/antiamazon/vendeur1/Table/Vendeur.java
+++ src/main/java/com/antiamazon/vendeur1/Table/Vendeur.java
@@ -1,5 +1,7 @@
 package com.antiamazon.vendeur1.Table;
 
+import com.antiamazon.vendeur1.entity.Adresse;
+
 public class Vendeur {
 
 }
--- src/main/java/com/antiamazon/vendeur1/controller/HomeController.java
+++ src/main/java/com/antiamazon/vendeur1/controller/HomeController.java
@@ -1,27 +1,87 @@
 package com.antiamazon.vendeur1.controller;
 
+import com.antiamazon.vendeur1.entity.Vendeur;
+import com.antiamazon.vendeur1.entity.Adresse;
+import com.antiamazon.vendeur1.entity.Paiement;
+import com.antiamazon.vendeur1.model.FillVendeurModel;
+import com.antiamazon.vendeur1.repository.VendeurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMessage;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.antiamazon.vendeur1.Table.NewVendeur;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class HomeController {
 
+
+    @Autowired
+    VendeurRepository vendeurRepository;
+
     @GetMapping(value = "/home", produces = "application/json")
-    public Map<String, String> get(){
+    public Map<String, String> get() {
         return Collections.singletonMap("message", "Bonne année");
     }
     
-    @PostMapping(value = "/fill", consumes = MediaType.APPLICATION_JSON_VALUE)
+    /*@PostMapping(value = "/fill", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String getVendeur(@RequestBody NewVendeur newVendeur){
     	return newVendeur.toString();
+    }*/
+
+    @PostMapping(value = "/fill", consumes = "application/json")
+    public ResponseEntity register(@RequestBody FillVendeurModel model) {
+
+        if (model.getSoc().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Le nom de la société ne peut pas être vide");
+        }
+        if (model.getRue().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Le nom de la rue ne peut pas être vide");
+        }
+        if (model.getVille().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Le nom de la ville ne peut pas être vide");
+        }
+        if (model.getCp() != (int) model.getCp()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Le nom de la ville ne peut pas être vide");
+        }
+        // Creation de l'adresse
+        Adresse adresse = new Adresse();
+        adresse.setRue(model.getRue());
+        adresse.setVille(model.getVille());
+        adresse.setCode_postal(model.getCp());
+
+        // Creation des paiements
+        Paiement paiementCB = new Paiement();
+        Paiement paiementPayPal = new Paiement();
+        Paiement paiementCheque = new Paiement();
+        List<Paiement> paiements = new ArrayList<Paiement>();
+        if (model.getPaiementCB() != null && !model.getPaiementCB().equals("")) {
+            paiementCB.setLibelle(model.getPaiementCB());
+            paiements.add(paiementCB);
+        }
+        if (model.getPaiementPaypal() != null && !model.getPaiementPaypal().equals("")) {
+            paiementPayPal.setLibelle(model.getPaiementPaypal());
+            paiements.add(paiementPayPal);
+        }
+        if (model.getPaiementCheque() != null && !model.getPaiementCheque().equals("")) {
+            paiementCheque.setLibelle(model.getPaiementCheque());
+            paiements.add(paiementCheque);
+        }
+
+        // Creation du vendeur / Mapping
+        Vendeur vendeur = new Vendeur(adresse, model.getSoc(), model.getDesc(), model.getPhoto64().getBytes());
+        vendeur.setPaiements(paiements);
+
+        vendeurRepository.save(vendeur);
+        return new ResponseEntity<Vendeur>(vendeur, HttpStatus.OK);
     }
 
 }
